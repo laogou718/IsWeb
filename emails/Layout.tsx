5b58861007163ba50867e95c34f545c074b8fe51
laogou718 <139377884+laogou718@users.noreply.github.com>
2023-12-30 16:24:11 +0800

    Update Layout.tsx

--- emails/Layout.tsx
+++ emails/Layout.tsx
@@ -39,43 +39,43 @@ export default function Layout({
                 src={`${emailConfig.baseUrl}/icon.png`}
                 width="24"
                 height="24"
-                alt="Cali"
+                alt="LAOGOU"
                 className="mx-auto my-0"
               />
               <Text className="text-center">
                 <Link
-                  href="https://cali.so"
+                  href="https://laogou717.com"
                   className="text-zinc-700 underline"
                 >
-                  <strong>Cali Castle</strong>
+                  <strong>LAOGOU717</strong>
                 </Link>
                 <br />
-                开发者、设计师、细节控、创始人
+                一个兴趣使然的UP主。
               </Text>
               <Text className="text-center">
                 <Link
-                  href="https://cali.so/twitter"
+                  href="https://twitter.com/fengduan11"
                   className="text-xs text-zinc-600 underline"
                 >
                   Twitter
                 </Link>{' '}
                 |&nbsp;
                 <Link
-                  href="https://cali.so/youtube"
+                  href="https://www.youtube.com/channel/UCUuwwXFGK8Z3OBrq6PzkmUg"
                   className="text-xs text-zinc-600 underline"
                 >
                   YouTube
                 </Link>{' '}
                 |&nbsp;
                 <Link
-                  href="https://cali.so/github"
+                  href="https://github.com/laogou717"
                   className="text-xs text-zinc-600 underline"
                 >
                   GitHub
                 </Link>{' '}
                 |&nbsp;
                 <Link
-                  href="https://cali.so/bilibili"
+                  href="https://space.bilibili.com/46377861?spm_id_from=333.1007.0.0"
                   className="text-xs text-zinc-600 underline"
                 >
                   哔哩哔哩
