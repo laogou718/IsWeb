611934fb2b94a8bc8b18b1b7a42ecf06b29d052a
laogou718 <139377884+laogou718@users.noreply.github.com>
2023-12-30 16:12:23 +0800

    Update seo.ts

--- lib/seo.ts
+++ lib/seo.ts
@@ -1,10 +1,10 @@
 export const seo = {
-  title: 'Cali Castle | 开发者、设计师、细节控、创始人',
+  title: 'LAOGOU | 一切开始的地方',
   description:
-    '我叫 Cali，一名开发者，设计师，细节控，同时也是佐玩创始人，目前带领着佐玩致力于创造一个充满创造力的工作环境，同时鼓励团队创造影响世界的产品。',
+    '欢迎来到LAOGOU717.COM ！一起见证宇宙最后的余晖。',
   url: new URL(
     process.env.NODE_ENV === 'production'
-      ? 'https://cali.so'
+      ? 'https://laogou717.com'
       : 'http://localhost:3000'
   ),
 } as const
